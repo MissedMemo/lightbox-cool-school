4946324c694daa13ec5e2b627cb971bf96f154c9
AndyL <missedmemo@gmail.com>
2016-06-17 10:54:24 -0700

    add search form button state, submit & edit handlers

--- client/src/components/searchbar.jsx
+++ client/src/components/searchbar.jsx
@@ -2,14 +2,40 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: ''
+    }
+
+    this.onEditText = this.onEditText.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onEditText(e) {
+    this.setState({ searchText: e.target.value })
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+    console.log('submitting...');
+    //this.props.getImages( this.state.searchText );
+  }
+
   render() {
-    return <form className='search-form'>
-      <input type='text'
-        placeholder='enter image search conditions...'
-        // value set to local state...
-      />
-      <button type='submit'>Find</button>
-    </form>;
+    return (
+      <form className='search-form' onSubmit={ this.onFormSubmit } >
+        <input type='text'
+          placeholder='enter image search conditions...'
+          onChange={ this.onEditText }
+          value={ this.state.searchText }
+        />
+        <button type='submit' disabled={ !this.state.searchText }>
+          Find
+        </button>
+      </form>
+    );
   }
 };
 
