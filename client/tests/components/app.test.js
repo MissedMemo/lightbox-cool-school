f4dc5600c60e7d0ee142d9cbbc3088df07e64fb2
AndyL <missedmemo@gmail.com>
2016-07-05 15:47:41 -0700

    validate presence of all components that make up the main layout

--- client/tests/components/app.test.js
+++ client/tests/components/app.test.js
@@ -4,13 +4,41 @@ import {shallow} from 'enzyme';
 
 import App from '../../src/components/app';
 import Header from '../../src/components/header';
+import SettingsPanel from '../../src/components/settings-panel';
+import SearchResults from '../../src/containers/search-results';
+import MessagePanel from '../../src/containers/message-panel';
+import LightBox from '../../src/components/lightbox';
 
 test( 'Main layout...', assert => {
 
-  assert.test( '  - should display a fat header', assert => {
+  assert.test( '  - should contain a fat header', assert => {
     assert.plan(1);
     const node = shallow( <App /> );
     assert.equal( node.find( Header ).length, 1 );
+  });
+
+  assert.test( '  - should contain a settings panel', assert => {
+    assert.plan(1);
+    const node = shallow( <App /> );
+    assert.equal( node.find( SettingsPanel ).length, 1 );
+  });
+
+  assert.test( '  - should contain a search results area', assert => {
+    assert.plan(1);
+    const node = shallow( <App /> );
+    assert.equal( node.find( SearchResults ).length, 1 );
+  });
+
+  assert.test( '  - should contain a message panel', assert => {
+    assert.plan(1);
+    const node = shallow( <App /> );
+    assert.equal( node.find( MessagePanel ).length, 1 );
+  });
+
+  assert.test( '  - should contain a LightBox!', assert => {
+    assert.plan(1);
+    const node = shallow( <App /> );
+    assert.equal( node.find( LightBox ).length, 1 );
   });
 
 });
