ec9d006114551506d6bb7e17c73f19519ad273ae
AndyL <missedmemo@gmail.com>
2016-07-05 15:31:55 -0700

    add enzyme 'shallow rendering' test support

--- client/tests/components/app.test.js
+++ client/tests/components/app.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import test from 'tape';
+import {shallow} from 'enzyme';
+
+import App from '../../src/components/app';
+import Header from '../../src/components/header';
 
 test( 'Main layout...', assert => {
 
   assert.test( '  - should display a fat header', assert => {
-    assert.equal( 2, 1+1 );
-    assert.end();
+    assert.plan(1);
+    const node = shallow( <App /> );
+    assert.equal( node.find( Header ).length, 1 );
   });
 
 });
